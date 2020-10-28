5dce228c6423627fa3d652db077fd7a7606d3177
Vitaly Simakov <faetonvs@gmail.com>
2020-10-28 15:54:06 +0500

    Update and rename array_while_test.rb to fast_while_test.rb

--- test/fast_while_test.rb
+++ test/fast_while_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
-class ArrayWhileTest < Minitest::Test
+class FastWhileTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::ArrayWhile::VERSION
+    refute_nil ::FastWhile::VERSION
   end
 
   def test_it_does_something_useful
